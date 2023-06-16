d5f2388e9b78fdc5fb64d62eb76e3cad18d6b179
hi-aa <ost.jj1201@gmail.com>
2023-06-16 21:01:49 +0900

    item select, clear

--- src/components/menu1/Item.js
+++ src/components/menu1/Item.js
@@ -1,7 +1,12 @@
 import PropTypes from "prop-types";
-import { styles } from './Item.module.css';
+import styles from './Item.module.css';
 
-function Item({list}) {
+function Item({ list, selectedKey, setSelectedKey, clearList }) {
+  // 아이템 선택
+  const onClick = (e, key) => {
+    setSelectedKey(selectedKey !== key ? key : -1); // 같은 아이템을 클릭하면 selected 해제
+  }
+
   return (
     <div>
       <table>
@@ -14,23 +19,28 @@ function Item({list}) {
         </thead>
         <tbody>
           {
-            list.map((v, i) => 
-            <tr key={i}>
-              <td>
-                {v}
-              </td>
-            </tr>  
+            list.map(item => 
+              <tr key={item.key}
+                onClick={e => onClick(e, item.key)}
+                className={[selectedKey === item.key ? styles.active: '', clearList.indexOf(item.key) > -1 ? styles.check : ''].join(' ')}
+              >
+                <td>
+                    {item.key} / {item.value}
+                  </td>
+              </tr>  
             )
           }
         </tbody>
       </table>
-
     </div>
   );
 }
 
 Item.propType = {
-  list: PropTypes.array.isRequired
+  list: PropTypes.array.isRequired,
+  selectedKey: PropTypes.number,
+  setSelectedKey: PropTypes.func.isRequired,
+  clearList: PropTypes.array.isRequired,
 };
 
 export default Item;
--- src/routes/Menu1/Menu1.js
+++ src/routes/Menu1/Menu1.js
@@ -6,10 +6,12 @@ import { useState } from "react";
 
 function Menu1() {
   const [list, setList] = useState([]);
+  const [counter, setCouter] = useState(0); // list key 생성용
+  const [selectedKey, setSelectedKey] = useState(-1);
+  const [clearList, setClearList] = useState([]); // checked list
   const [alert, setAlert] = useState('');
 
-  // 여러개의 input의 상태를 연결하기 
-  const [inputs, setInputs] = useState({
+  const [inputs, setInputs] = useState({ // 여러개의 input의 상태를 연결하기 
     menu1: ''
   });
 
@@ -26,46 +28,90 @@ function Menu1() {
   // 리스트 추가
   const addMenu = (e) => {
     if(inputs.menu1 === "") { // 공백 막기
-      setAlert('내용을 작성하세요.');
-      const timer = setTimeout(function() {
-        setAlert(''); // 클리어
-      }, 3000);
-      return () => clearTimeout(timer);
+      showAlert('내용을 작성하세요.');
+      return;
     }
-    setList([...list, inputs.menu1]); // 리스트 업데이트
+    setList([...list, {key: counter, value: inputs.menu1}]); // 리스트 업데이트
     setInputs({...inputs, menu1 : ""}); // input 클리어
+    setCouter(counter => ++counter);
   }
 
   // 리스트 삭제
   const deleteMenu = () => {
-    console.log("delete");
+    if(selectedKey < 0) {
+      showAlert('삭제할 아이템을 선택하세요.');
+      return;
+    }
+
+    let temp = [...list]; // 배열 복사
+    let selectedIdx = list.findIndex(v => v.key === selectedKey);
+    temp.splice(selectedIdx, 1); // 복사된 배열에서 selectedKey 삭제
+    // state 업데이트
+    setList(temp); 
+    setSelectedKey(-1);
   }
 
   // 리스트 체크
   const checkMenu = () => {
-    console.log("check");
+    if(selectedKey < 0) {
+      showAlert('체크할 아이템을 선택하세요.');
+      return;
+    }
+
+    let checkIdx = clearList.indexOf(selectedKey);
+    if(checkIdx === -1) { // 추가
+      console.log('추가')
+      setClearList([...clearList, selectedKey]);
+      
+    } else { // 삭제
+      console.log('삭제')
+
+      let temp = [...clearList];
+      temp.splice(checkIdx, 1);
+
+      setClearList(temp);
+
+    }
+    console.log(clearList)
+
+    // setClearList()
+  }
+
+  // 알럿 표시
+  const showAlert = (message) => {
+    setAlert(message);
+
+    const timer = setTimeout(function() {
+      setAlert(''); // 클리어
+    }, 3000);
+    return () => clearTimeout(timer);
   }
 
   return (
     <div>
       <div className="btn-wrap">
         <br/>
-          <input type="text" name="menu1" value={inputs.menu1} onChange={onChangeInputs} />
+        {/* [{selectedKey}] */}
+        <input type="text" name="menu1" value={inputs.menu1} onChange={onChangeInputs} />
 
-        <button className={styles['btn']} onClick={addMenu}>
+        <button className={styles['btn']} onClick={addMenu} title="Add item">
           <FontAwesomeIcon icon={faPlus} size="2xl"/>
         </button>
-        <button className={styles['btn']} onClick={deleteMenu}>
+        <button className={styles['btn']} onClick={deleteMenu} title="Delete item">
           <FontAwesomeIcon icon={faXmark} size="2xl"/>
         </button>
-        <button className={styles['btn']} onClick={checkMenu}>
+        <button className={styles['btn']} onClick={checkMenu} title="Check item">
           <FontAwesomeIcon icon={faCheck} size="2xl"/>
         </button>
       </div>
       <div className={styles['alert-wrap']}>{alert}</div>
 
       <div className="cont-wrap">
-        <Item list={list} />
+        <Item list={list}
+          selectedKey={selectedKey}
+          setSelectedKey={setSelectedKey}
+          clearList={clearList}
+        />
       </div>
     </div>
   );
