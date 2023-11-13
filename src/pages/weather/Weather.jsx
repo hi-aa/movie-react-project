20155a760d482023d54d686dff8b797c65c7d381
hi-aa <ost.jj1201@gmail.com>
2023-11-13 21:18:32 +0900

    임시저장

--- src/pages/weather/Weather.jsx
+++ src/pages/weather/Weather.jsx
@@ -1,12 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./weather.css";
 import { fetchWeatherList } from "../../api/weather-api";
+import { WTHR_AREA_CODE } from "../../constants/weather-enum";
 export default function Weather() {
+  const [weatherLoc, setWeatherLoc] = useState(""); // 지역명
+  const [weatherList, setWeatherList] = useState([]); // 일자별 날씨정보
+  const iconList = []; // 날씨 아이콘
+
   useEffect(() => {
     fetchWeatherList().then((res) => {
-      const { items } = res;
-      const info = items?.item?.[0];
-      console.log(info);
+      const { regId, ...item } = res?.items?.item?.[0];
+      // 지역명
+      const locKey = Object.keys(WTHR_AREA_CODE).find(
+        (v) => WTHR_AREA_CODE[v].code === regId
+      );
+      setWeatherLoc(WTHR_AREA_CODE[locKey].label);
+
+      // 일자별 날씨정보
+      const weatherKeys = Object.keys(item);
+      const range = Array(8)
+        .fill(0)
+        .map((_, i) => i + 3); // 3 ~ 10일까지
+
+      // TODO : key에 숫자
+      const list = range.map((day) => {
+        const dayInfo = weatherKeys
+          .filter((v2) => v2.indexOf(day) !== -1)
+          .map((v) => {
+            console.log(item[v]);
+            return v;
+          });
+        return day;
+      });
+      console.log(list);
     });
   }, []);
   return (
@@ -23,7 +49,7 @@ export default function Weather() {
               </div>
               <div class="p-3">
                 <h5>Tuesday, 10 AM</h5>
-                <h3>San Francisco</h3>
+                <h3>{weatherLoc}</h3>
                 <span class="weather__description">Mostly Cloudy</span>
               </div>
             </div>
