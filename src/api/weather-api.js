184769710cc6a1db0cb5ceb408f3b977c1cd23fa
hi-aa <ost.jj1201@gmail.com>
2023-11-14 21:08:21 +0900

    기온 조회, 목록 생성

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -3,10 +3,21 @@ import { api } from ".";
 import { WTHR_AREA_CODE } from "../constants/weather-enum";
 
 // 중기육상예보조회 (3 ~ 10일)
-export const fetchWeatherList = async () => {
-  const url = `${process.env.REACT_APP_WEATHER_API}`;
+export const fetchWeatherLandList = async () => {
+  const url = `${process.env.REACT_APP_MID_WEATHER_LAND_API}`;
   const tmFc = `${dayjs().format("YYYYMMDD")}0600`;
-  const area = WTHR_AREA_CODE.SEOUL.code;
+  const area = WTHR_AREA_CODE.SEOUL.landCode;
+  const params = `serviceKey=${process.env.REACT_APP_WEATHER_API_KEY}&numOfRows=7&pageNo=1&dataType=JSON&regId=${area}&tmFc=${tmFc}`;
+
+  const { response } = await api.get(`${url}?${params}`);
+  return response?.body;
+};
+
+// 중기기온조회 (3 ~ 10일)
+export const fetchWeatherList = async () => {
+  const url = `${process.env.REACT_APP_MID_WEATHER_API}`;
+  const tmFc = `${dayjs().format("YYYYMMDD")}0600`;
+  const area = WTHR_AREA_CODE.SEOUL.tmprCode;
   const params = `serviceKey=${process.env.REACT_APP_WEATHER_API_KEY}&numOfRows=7&pageNo=1&dataType=JSON&regId=${area}&tmFc=${tmFc}`;
 
   const { response } = await api.get(`${url}?${params}`);
--- src/pages/weather/Weather.jsx
+++ src/pages/weather/Weather.jsx
@@ -1,68 +1,223 @@
 import { useEffect, useState } from "react";
 import "./weather.css";
-import { fetchWeatherList } from "../../api/weather-api";
+import { fetchWeatherLandList, fetchWeatherList } from "../../api/weather-api";
 import { WTHR_AREA_CODE } from "../../constants/weather-enum";
+import dayjs from "dayjs";
+
 export default function Weather() {
   const [weatherLoc, setWeatherLoc] = useState(""); // 지역명
   const [weatherList, setWeatherList] = useState([]); // 일자별 날씨정보
   const iconList = []; // 날씨 아이콘
 
+  // test
   useEffect(() => {
+    console.log(weatherList);
+  }, [weatherList]);
+
+  // weatherList 날짜별로 합치기
+  const appendWeatherList = (list = []) => {
+    setWeatherList((prev) => {
+      const prevYmdArr = prev.map((item) => item.ymd);
+      const newYmdArr = list.map((item) => item.ymd);
+      let ymdList = new Set([...prevYmdArr, ...newYmdArr]); // 중복제거
+
+      ymdList = [...ymdList].map((v) => {
+        const prevObj = prev.find((item) => item.ymd === v);
+        const newObj = list.find((item) => item.ymd === v);
+        return { ...prevObj, ...newObj };
+      });
+      console.log({ ymdList });
+      return ymdList;
+    });
+  };
+
+  // init
+  useEffect(() => {
+    // 강수량, 날씨
+    fetchWeatherLandList().then((res) => {
+      const { regId, ...item } = res?.items?.item?.[0];
+      // 일자별 날씨정보
+      const list = [
+        {
+          ymd: dayjs().add(3, "day").format("YYYYMMDD"),
+          rnStAm: item.rnSt3Am,
+          rnStPm: item.rnSt3Pm,
+          wfAm: item.wf3Am,
+          wfPm: item.wf3Pm,
+          rnSt: item.rnSt3Am,
+          wf: item.wf3Am,
+        },
+        {
+          ymd: dayjs().add(4, "day").format("YYYYMMDD"),
+          rnStAm: item.rnSt4Am,
+          rnStPm: item.rnSt4Pm,
+          wfAm: item.wf4Am,
+          wfPm: item.wf4Pm,
+          rnSt: item.rnSt4Am,
+          wf: item.wf4Am,
+        },
+        {
+          ymd: dayjs().add(5, "day").format("YYYYMMDD"),
+          rnStAm: item.rnSt5Am,
+          rnStPm: item.rnSt5Pm,
+          wfAm: item.wf5Am,
+          wfPm: item.wf5Pm,
+          rnSt: item.rnSt5Am,
+          wf: item.wf5Am,
+        },
+        {
+          ymd: dayjs().add(6, "day").format("YYYYMMDD"),
+          rnStAm: item.rnSt6Am,
+          rnStPm: item.rnSt6Pm,
+          wfAm: item.wf6Am,
+          wfPm: item.wf6Pm,
+          rnSt: item.rnSt6Am,
+          wf: item.wf6Am,
+        },
+        {
+          ymd: dayjs().add(7, "day").format("YYYYMMDD"),
+          rnStAm: item.rnSt7Am,
+          rnStPm: item.rnSt7Pm,
+          wfAm: item.wf7Am,
+          wfPm: item.wf7Pm,
+          rnSt: item.rnSt7Am,
+          wf: item.wf7Am,
+        },
+        {
+          ymd: dayjs().add(8, "day").format("YYYYMMDD"),
+          rnSt: item.rnSt8,
+          wf: item.wf8,
+        },
+        {
+          ymd: dayjs().add(9, "day").format("YYYYMMDD"),
+          rnSt: item.rnSt9,
+          wf: item.wf9,
+        },
+        {
+          ymd: dayjs().add(10, "day").format("YYYYMMDD"),
+          rnSt: item.rnSt10,
+          wf: item.wf10,
+        },
+      ];
+      appendWeatherList(list);
+    });
+
+    // 기온
     fetchWeatherList().then((res) => {
       const { regId, ...item } = res?.items?.item?.[0];
       // 지역명
       const locKey = Object.keys(WTHR_AREA_CODE).find(
-        (v) => WTHR_AREA_CODE[v].code === regId
+        (v) => WTHR_AREA_CODE[v].tmprCode === regId
       );
-      setWeatherLoc(WTHR_AREA_CODE[locKey].label);
+      setWeatherLoc(WTHR_AREA_CODE[locKey].tmprLabel);
 
       // 일자별 날씨정보
-      const weatherKeys = Object.keys(item);
-      const range = Array(8)
-        .fill(0)
-        .map((_, i) => i + 3); // 3 ~ 10일까지
-
-      // TODO : key에 숫자
-      const list = range.map((day) => {
-        const dayInfo = weatherKeys
-          .filter((v2) => v2.indexOf(day) !== -1)
-          .map((v) => {
-            console.log(item[v]);
-            return v;
-          });
-        return day;
-      });
-      console.log(list);
+      const list = [
+        {
+          ymd: dayjs().add(3, "day").format("YYYYMMDD"),
+          taMin: item.taMin3,
+          taMinLow: item.taMin3Low,
+          taMinHigh: item.taMin3High,
+          taMax: item.taMax3,
+          taMaxLow: item.taMax3Low,
+          taMaxHigh: item.taMax3High,
+        },
+        {
+          ymd: dayjs().add(4, "day").format("YYYYMMDD"),
+          taMin: item.taMin4,
+          taMinLow: item.taMin4Low,
+          taMinHigh: item.taMin4High,
+          taMax: item.taMax4,
+          taMaxLow: item.taMax4Low,
+          taMaxHigh: item.taMax4High,
+        },
+        {
+          ymd: dayjs().add(5, "day").format("YYYYMMDD"),
+          taMin: item.taMin5,
+          taMinLow: item.taMin5Low,
+          taMinHigh: item.taMin5High,
+          taMax: item.taMax5,
+          taMaxLow: item.taMax5Low,
+          taMaxHigh: item.taMax5High,
+        },
+        {
+          ymd: dayjs().add(6, "day").format("YYYYMMDD"),
+          taMin: item.taMin6,
+          taMinLow: item.taMin6Low,
+          taMinHigh: item.taMin6High,
+          taMax: item.taMax6,
+          taMaxLow: item.taMax6Low,
+          taMaxHigh: item.taMax6High,
+        },
+        {
+          ymd: dayjs().add(7, "day").format("YYYYMMDD"),
+          taMin: item.taMin7,
+          taMinLow: item.taMin7Low,
+          taMinHigh: item.taMin7High,
+          taMax: item.taMax7,
+          taMaxLow: item.taMax7Low,
+          taMaxHigh: item.taMax7High,
+        },
+        {
+          ymd: dayjs().add(8, "day").format("YYYYMMDD"),
+          taMin: item.taMin8,
+          taMinLow: item.taMin8Low,
+          taMinHigh: item.taMin8High,
+          taMax: item.taMax8,
+          taMaxLow: item.taMax8Low,
+          taMaxHigh: item.taMax8High,
+        },
+        {
+          ymd: dayjs().add(9, "day").format("YYYYMMDD"),
+          taMin: item.taMin9,
+          taMinLow: item.taMin9Low,
+          taMinHigh: item.taMin9High,
+          taMax: item.taMax9,
+          taMaxLow: item.taMax9Low,
+          taMaxHigh: item.taMax9High,
+        },
+        {
+          ymd: dayjs().add(10, "day").format("YYYYMMDD"),
+          taMin: item.taMin10,
+          taMinLow: item.taMin10Low,
+          taMinHigh: item.taMin10High,
+          taMax: item.taMax10,
+          taMaxLow: item.taMax10Low,
+          taMaxHigh: item.taMax10High,
+        },
+      ];
+      appendWeatherList(list);
     });
   }, []);
   return (
     <>
+      {/* 상단 대표영역 */}
       <div className="container mt-5">
-        <div class="d-flex flex-row justify-content-center align-items-center">
-          <div class="weather__card">
-            <div class="d-flex flex-row justify-content-center align-items-center">
-              <div class="p-3">
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <div className="weather__card">
+            <div className="d-flex flex-row justify-content-center align-items-center">
+              <div className="p-3">
                 <h2>15&deg;</h2>
               </div>
-              <div class="p-3">
+              <div className="p-3">
                 <img src="https://svgur.com/i/oKG.svg" />
               </div>
-              <div class="p-3">
+              <div className="p-3">
                 <h5>Tuesday, 10 AM</h5>
                 <h3>{weatherLoc}</h3>
-                <span class="weather__description">Mostly Cloudy</span>
+                <span className="weather__description">Mostly Cloudy</span>
               </div>
             </div>
-            <div class="weather__status d-flex flex-row justify-content-center align-items-center mt-3">
-              <div class="p-4 d-flex justify-content-center align-items-center">
+            <div className="weather__status d-flex flex-row justify-content-center align-items-center mt-3">
+              <div className="p-4 d-flex justify-content-center align-items-center">
                 <img src="https://svgur.com/i/oHw.svg" />
                 <span>10%</span>
               </div>
-              <div class="p-4 d-flex justify-content-center align-items-center">
+              <div className="p-4 d-flex justify-content-center align-items-center">
                 <img src="https://svgur.com/i/oH_.svg" />
                 <span>0.53 mB</span>
               </div>
-              <div class="p-4 d-flex justify-content-center align-items-center">
+              <div className="p-4 d-flex justify-content-center align-items-center">
                 <img src="https://svgur.com/i/oKS.svg" />
                 <span>10 km/h</span>
               </div>
@@ -71,57 +226,57 @@ export default function Weather() {
         </div>
       </div>
 
-      <div class="weather__forecast d-flex flex-row justify-content-center align-items-center mt-3">
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+      {/* 목록 */}
+      <div className="weather__forecast d-flex flex-row justify-content-center align-items-center mt-3">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Wed</span>
           <img src="https://svgur.com/i/oJe.svg" />
           <span>13&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Thu</span>
           <img src="https://svgur.com/i/oKG.svg" />
           <span>15&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Wed</span>
           <img src="https://svgur.com/i/oKG.svg" />
           <span>15&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Fri</span>
           <img src="https://svgur.com/i/oJe.svg" />
           <span>13&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Sat</span>
           <img src="https://svgur.com/i/oJx.svg" />
           <span>13&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Sun</span>
           <img src="https://svgur.com/i/oJU.svg" />
           <span>11&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Mon</span>
           <img src="https://svgur.com/i/oJU.svg" />
           <span>11&deg;</span>
         </div>
 
-        <div class="p-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="p-4 d-flex flex-column justify-content-center align-items-center">
           <span>Tue</span>
           <img src="https://svgur.com/i/oJy.svg" />
           <span>6&deg;</span>
         </div>
       </div>
-
-      {/* <div class="mt-5 d-flex justify-content-center align-items-center">
+      {/* <div className="mt-5 d-flex justify-content-center align-items-center">
         Made with ♡ by <a href="https://twitter.com/leutrimdemirii"> Leutrim</a>
       </div> */}
     </>
