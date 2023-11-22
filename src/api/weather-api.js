92103b5f018d522507e27200f29eac0dc5c12b7f
hi-aa <ost.jj1201@gmail.com>
2023-11-22 21:07:50 +0900

    지역변경 select 추가

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -3,10 +3,10 @@ import { api } from ".";
 import { WTHR_AREA_CODE } from "../constants/weather-enum";
 
 // 중기육상예보조회 (3 ~ 10일)
-export const fetchWeatherLandList = async () => {
+export const fetchWeatherLandList = async (key = "SEOUL") => {
   const url = `${process.env.REACT_APP_MID_WEATHER_LAND_API}`;
   const tmFc = `${dayjs().format("YYYYMMDD")}0600`;
-  const area = WTHR_AREA_CODE.SEOUL.landCode;
+  const area = WTHR_AREA_CODE[key].landCode;
   const params = `serviceKey=${process.env.REACT_APP_WEATHER_API_KEY}&numOfRows=7&pageNo=1&dataType=JSON&regId=${area}&tmFc=${tmFc}`;
 
   const { response } = await api.get(`${url}?${params}`);
@@ -14,10 +14,10 @@ export const fetchWeatherLandList = async () => {
 };
 
 // 중기기온조회 (3 ~ 10일)
-export const fetchWeatherList = async () => {
+export const fetchWeatherList = async (key = "SEOUL") => {
   const url = `${process.env.REACT_APP_MID_WEATHER_API}`;
   const tmFc = `${dayjs().format("YYYYMMDD")}0600`;
-  const area = WTHR_AREA_CODE.SEOUL.tmprCode;
+  const area = WTHR_AREA_CODE[key].tmprCode;
   const params = `serviceKey=${process.env.REACT_APP_WEATHER_API_KEY}&numOfRows=7&pageNo=1&dataType=JSON&regId=${area}&tmFc=${tmFc}`;
 
   const { response } = await api.get(`${url}?${params}`);
